2d9936238e70a74fe81ca69448e14e7539ce11b1
Unknown <maya.karabala8@gmail.com>
2019-05-07 19:58:34 +0300

    commecnts

--- splitServer/src/splitServer/SClient.java
+++ splitServer/src/splitServer/SClient.java
@@ -24,13 +24,10 @@ public class SClient {
     Socket soket;
     ObjectOutputStream sOutput;
     ObjectInputStream sInput;
-    //clientten gelenleri dinleme threadi
     Listen listenThread;
-    //cilent eşleştirme thredi
-    //PairingThread pairThread;
-    //rakip client
+
     SClient rival;
-    //eşleşme durumu
+
     public boolean paired = false;
 
     public SClient(Socket gelenSoket, int id) {
@@ -42,9 +39,8 @@ public class SClient {
         } catch (IOException ex) {
             Logger.getLogger(SClient.class.getName()).log(Level.SEVERE, null, ex);
         }
-        //thread nesneleri
         this.listenThread = new Listen(this);
-        //this.pairThread = new PairingThread(this);
+
 
     }
 
@@ -72,10 +68,10 @@ public class SClient {
                     //mesajı bekleyen kod satırı
                     msg=(TheClient.sInput.readObject());
                     if(msg instanceof Bill){
-                        
+                     // if msg type is bill         
                         receivedBill=(Bill)msg;
                         receivedBill.ammountToPaid= receivedBill.amount/(receivedBill.reciver.members.size()+1);
-                        // sendin bill to group members
+                        // send bill to group members
                          for (SClient Client : Server.Clients) {
                               for (String member : receivedBill.reciver.members) {
                                   // if clent name in bill groubs number send bill
@@ -106,18 +102,10 @@ public class SClient {
                     else{
                       
                     received = (Message) msg;
-                    // determining reciver
-
-                    //////////////////////////////
-                    
-                    //mesaj gelirse bu satıra geçer
-                    //mesaj tipine göre işlemlere ayır
 
                     if (received.type == Name) {
+                        // add clents with uniqu user name
                         TheClient.name = received.content.get(0).toString();
-                        //clienti listeye ekle.
-
-                        
                         boolean containsSameName = false;
                         for (SClient Client : Server.Clients) {
                             if (Client.name.equals(TheClient.name)) {
@@ -147,7 +135,7 @@ public class SClient {
 
                     } else {
                         for (SClient clnt : Server.Clients) {
-                            System.out.println("clnt.name " + clnt.name);
+                           
                             if (clnt.name.equals(received.reciverName)) {
                                 reciver = clnt;
                             }
@@ -172,70 +160,3 @@ public class SClient {
     }
 }
 
-//eşleştirme threadi
-//her clientin ayrı bir eşleştirme thredi var
-/*
-class PairingThread extends Thread {
-    SClient TheClient;
-    PairingThread(SClient TheClient) {
-        this.TheClient = TheClient;
-    }
-    public void run() {
-        //client bağlı ve eşleşmemiş olduğu durumda dön
-        while (TheClient.soket.isConnected() && TheClient.paired == false) {
-            try {
-                //lock mekanizması
-                //sadece bir client içeri grebilir
-                //diğerleri release olana kadar bekler
-                Server.pairTwo.acquire(1);
-
-                //client eğer eşleşmemişse gir
-                if (!TheClient.paired) {
-                    SClient crival = null;
-                    //eşleşme sağlanana kadar dön
-                    while (crival == null && TheClient.soket.isConnected()) {
-                        //liste içerisinde eş arıyor
-                        for (SClient clnt : Server.Clients) {
-                            // clientin esi yoksa ve bu thredi çaliştıran claient değilse
-                            if (TheClient != clnt && clnt.rival == null) {
-                                //eşleşme sağlandı ve gerekli işaretlemeler yapıldı
-                                crival = clnt;
-                                crival.paired = true;
-                                crival.rival = TheClient;
-
-                                TheClient.rival = crival;
-                                TheClient.paired = true;
-
-                                break;
-                            }
-                        }
-                        //sürekli dönmesin 1 saniyede bir dönsün
-                        //thredi uyutuyoruz
-                        sleep(1000);
-                    }
-                    //eşleşme oldu
-                    //her iki tarafada eşleşme mesajı gönder 
-                    //oyunu başlat
-                    Message msg1 = new Message(Message.Message_Type.RivalConnected);
-                    msg1.content = TheClient.name;
-                    Server.Send(TheClient.rival, msg1);
-
-                    //change default color from red to yeloow
-                    Message msg12 = new Message(Message.Message_Type.ChangeColor);
-                    Server.Send(TheClient.rival, msg12);
-
-                    Message msg2 = new Message(Message.Message_Type.RivalConnected);
-                    msg2.content = TheClient.rival.name;
-                    Server.Send(TheClient, msg2);
-
-                }
-                //lock mekanizmasını servest bırak
-                //bırakılmazsa deadlock olur.
-                Server.pairTwo.release(1);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(PairingThread.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-}
- */
